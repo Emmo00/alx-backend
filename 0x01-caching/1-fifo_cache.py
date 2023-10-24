8a895c4460bf685393308a2a007157a3cb56c968
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-24 13:19:33 +0100

    kd

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,18 +15,18 @@ class FIFOCache(BaseCaching):
         """
         super().__init__()
         self.queue = {}
-        self.history_in = 0
+        self.first_in = 0
 
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key is None or item is None:
             return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMSi:
-            del self.cache_data[self.first_in]
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_in = list(self.cache_data.keys())[0]
+            print(f"DISCARD: {first_in}")
+            del self.cache_data[first_in]
         self.cache_data[key] = item
-        self.queue[self.history_index] = key
-        self.history_index += 1
 
     def get(self, key):
         """ Get an item by key
