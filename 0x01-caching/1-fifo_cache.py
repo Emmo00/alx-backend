7660c8e8dcc2cedcfe2854a92624728e95103e7d
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-24 05:03:47 -0700

    kd

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,16 +5,28 @@ basic caching
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """
-    Basic caching impl.
+    FIFO impl. caching
     """
+    def __init__(self):
+        """
+        initialize
+        """
+        super().__init__()
+        self.queue = {}
+        self.history_in = 0
+
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key is None or item is None:
             return
+        if len(self.cache_data) > BaseCaching.MAX_ITEMSi:
+            del self.cache_data[self.first_in]
         self.cache_data[key] = item
+        self.queue[self.history_index] = key
+        self.history_index += 1
 
     def get(self, key):
         """ Get an item by key
