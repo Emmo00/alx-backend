7c55468358a7a08d551211e0930fca4d6da8d376
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-30 21:57:17 +0100

    omo

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,20 +1,24 @@
 #!/usr/bin/env node
 import redis, { createClient } from "redis";
 
-const client = createClient();
-client.on("connect", () => console.log("Redis client connected to the server"));
-client.on("error", (err) =>
-  console.log("Redis client not connected to the server: ", err)
-);
+(function connectRedis() {
+  const client = createClient();
+  client.on("connect", () =>
+    console.log("Redis client connected to the server")
+  );
+  client.on("error", (err) =>
+    console.log("Redis client not connected to the server: ", err)
+  );
+})();
 
 function setNewSchool(schoolName, value) {
   client.set(schoolName, value, redis.print);
 }
 
 function displaySchoolValue(schoolName) {
-  console.log(client.get(schoolName))
+  console.log(client.get(schoolName));
 }
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+displaySchoolValue("Holberton");
+setNewSchool("HolbertonSanFrancisco", "100");
+displaySchoolValue("HolbertonSanFrancisco");
