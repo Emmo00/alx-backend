efec3f58fb88f053b3ce74143bac3b3cefbb8473
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-30 21:41:41 +0100

    redis

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -2,14 +2,13 @@
 import { createClient } from "redis";
 
 async function connectRedis() {
-  const client = await createClient()
-    .on("connect", () => console.log("Redis client connected to the server"))
-    .on("error", (err) =>
-      console.log("Redis client not connected to the server: ", err)
-    )
-    .connect();
-
-  await client.disconnect();
+  const client = createClient();
+  client.on("connect", () =>
+    console.log("Redis client connected to the server")
+  );
+  client.on("error", (err) =>
+    console.log("Redis client not connected to the server: ", err)
+  );
 }
 
 connectRedis();
