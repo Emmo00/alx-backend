f255c775125325ce8a2ed75608a26d2d55f87c83
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-12-02 02:56:13 -0800

    sub_pub

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
-async function connectRedis() {
+async function connectRedis () {
   const client = createClient();
-  client.on("connect", () =>
-    console.log("Redis client connected to the server")
+  client.on('connect', () =>
+    console.log('Redis client connected to the server')
   );
-  client.on("error", (err) =>
-    console.log("Redis client not connected to the server: ", err)
+  client.on('error', (err) =>
+    console.log('Redis client not connected to the server: ', err)
   );
 }
 
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -8,6 +8,7 @@ client.on('error', (err) =>
 );
 
 client.on('message', (channel, message) => {
+  console.log(message);
   if (message === 'KILL_SERVER') {
     client.unsubcribe(channel);
     process.exit();
