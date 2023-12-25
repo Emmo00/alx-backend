88efcd20c3517aef3ff8002686cf4063560592ac
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-12-25 13:28:51 +0100

    0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """index page
+    """
+    return render_template('0-index.html')
+
+app.run('0.0.0.0', 5000, debug=True)
