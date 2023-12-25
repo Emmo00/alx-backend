429cb043ebd474525acc39deb6d698b5203ee7f2
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-12-25 15:56:18 +0100

    2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""flask app
+"""
+from typing import List
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+class Config:
+    """config object
+    """
+    LANGUAGES: List[str] = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config())
+
+
+@app.route('/')
+def index() -> None:
+    """index page
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
