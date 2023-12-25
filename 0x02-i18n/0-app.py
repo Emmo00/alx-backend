58a8a04f09e808064857fdb7474ec43c69b13536
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-12-25 13:52:19 +0100

    pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """index page
     """
     return render_template('0-index.html')
+
 
 app.run('0.0.0.0', 5000, debug=True)
